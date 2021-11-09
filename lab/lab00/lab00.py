e940cff06889170d54977e08f490fd77b56cc7c7
Beixi Hao <beixi.hao@life-foundry.com>
2021-11-09 11:44:12 +0800

    add hw0 and lab0

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,8 @@
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2020,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty()
+    2120
+    """
+    return 1000+10*2+1000+100
